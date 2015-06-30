36a554fa0cc0dbcb52b7b8c5644b59d40f121f8d
nacho.iyaam@gmail.com <nacho.iyaam@gmail.com>
2015-07-01 00:44:45 +0100

    WpfSupportLibs

---
+++ WpfSupportLibs/WpfSupportLibs/Blah/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+
+namespace WpfSupportLibs.Blah
+{
+    public class Class1
+    {
+        [Test]
+        public void SomeTest()
+        {
+            var repo = new MockRepository(MockBehavior.Loose);
+        }
+    }
+}
